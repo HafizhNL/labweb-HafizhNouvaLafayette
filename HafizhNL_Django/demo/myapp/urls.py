a67b244ef96cc1ff37eb07f04c4e5082eb00971f
HafizhNL <hafizh.nouval25@gmail.com>
2025-09-18 16:38:44 +0700

    update

--- HafizhNL_Django/demo/myapp/urls.py
+++ HafizhNL_Django/demo/myapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.mahasiswa, name='mahasiswa'),
+    path('', views.daftar_mahasiswa, name='daftar_mahasiswa'),
 ]
--- HafizhNL_Django/demo/myapp/views.py
+++ HafizhNL_Django/demo/myapp/views.py
@@ -1,13 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Mahasiswa
-from django.template import loader
 from django.http import HttpResponse
 
 # Create your views here.
-def mahasiswa(request):
-    mymahasiswa = Mahasiswa.objects.all().values()
-    template = loader.get_template('view.html')
+
+def daftar_mahasiswa(request):
+    if request.method == 'POST':
+        action = request.POST.get('action')
+        
+        if action == 'add':
+            Mahasiswa.objects.create(
+                nim=request.POST.get('nim'),
+                firstname=request.POST.get('firstname'),
+                lastname=request.POST.get('lastname'),
+                jurusan=request.POST.get('jurusan')
+            )
+            
+        elif action == 'update':
+            mhs = Mahasiswa.objects.get(id=request.POST.get('id'))
+            mhs.nim = request.POST.get('nim')
+            mhs.firstname = request.POST.get('firstname')
+            mhs.lastname = request.POST.get('lastname')
+            mhs.jurusan = request.POST.get('jurusan')
+            mhs.save()
+            
+        elif action == 'delete':
+            mhs = Mahasiswa.objects.filter(id=request.POST.get('id')).delete()
+        
+        return redirect('daftar_mahasiswa')
+    mymahasiswa = Mahasiswa.objects.all()
     context = {
         'mymahasiswa': mymahasiswa,
     }
-    return HttpResponse(template.render(context, request))
+    
+    return render(request, 'view.html', context)
